231ed67e4d281faa0ee7668138679963e28e5609
吕建成 <59474029+SnowGuest@users.noreply.github.com>
2024-07-10 17:33:17 +0800

    添加详情

---
+++ src/components/bilibili.vue
@@ -0,0 +1,31 @@
+<template>
+    <iframe class="bilibili" :src="src" >
+    </iframe>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+    bvid?: string;
+    avid?: string;
+}
+
+const props = defineProps<Props>();
+const src = computed(() => {
+    const params: string[] = [];
+    if (props.avid) {
+        params.push(`aid=${props.avid}`)
+    }
+    if (props.bvid) {
+        params.push(`bvid=${props.bvid}`)
+    }
+    return `//player.bilibili.com/player.html?${params.join("&")}&danmaku=0&as_wide=1&high_quality=1`
+})
+</script>
+<style scoped lang="less">
+.bilibili {
+    width: 100%;
+    height: 400px;
+    max-width: 100vw;
+    margin: 0 auto;
+}
+</style>
--- src/pages/monf/[time,id].vue
+++ src/pages/monf/[time,id].vue
@@ -1,15 +1,34 @@
 <!--  -->
 <script lang="ts" setup>
-import { getMonf } from '@/apis/monf';
+import { getMonf, monfLike, monfunLike } from '@/apis/monf';
+import BilibiliCe from '@/components/bilibili.vue';
 import UseHead from '@/components/useHead.vue';
+import { useAppStore } from '@/stores/app';
+import { useUserStore } from '@/stores/users';
 import { message } from 'ant-design-vue';
 const route = useRoute()
 const router = useRouter()
-const monf = ref<Monf>()
-const loading = ref(true)
+const monf = ref<Monf>();
+const appStore = useAppStore();
+const userStore = useUserStore();
+const loading = ref(true);
+const columns = [{
+    title: '昵称',
+    dataIndex: 'memberName',
+    key: 'memberName',
+}, {
+    title: '职位',
+    dataIndex: 'memberJob',
+    key: 'memberJob',
+}, {
+    title: 'Malody UID',
+    dataIndex: 'memberJob',
+    key: 'memberJob',
+},]
 async function loadMonf(id: string) {
     try {
         const result = await getMonf(id);
+        userStore.setUsers(result.data.includes.users);
         monf.value = result.data.work
     } finally {
         loading.value = false
@@ -21,30 +40,126 @@ if (typeof route.params.id === "string") {
 } else {
     router.replace("/noPage")
 }
+async function unLike() {
+    if (!monf.value) return
+    await monfunLike(monf.value.id);
+    if (monf.value.relations) {
+        monf.value.relations.isLiked = false
+    }
+    if (monf.value.likeCount > 0) monf.value.likeCount--
+    message.info("取消点赞")
+}
 
+async function like() {
+    if (!monf.value) return
+    await monfLike(monf.value.id)
+    monf.value.likeCount++
+    if (monf.value.relations) {
+        monf.value.relations.isLiked = true
+    }
+    message.success("点赞成功")
+}
 </script>
 
 <template>
-    <a-page-header title="返回" @back=" $router.back" />
+    <a-page-header title="返回" @back=" $router.back" class="pageHeader" />
     <div class="skeleton" v-show="loading">
         <a-skeleton active />
     </div>
     <div class="container" v-if="!loading && monf">
         <div class="container-body">
-            <h2>{{ monf?.songName }}</h2>
-            <UseHead :createdUserId="monf.createdUserId" />
+            <h2 class="title">{{ monf.songName }}</h2>
+            <UseHead padding="0 0 20px 0" :createdUserId="monf.createdUserId" />
+            <h3 class="label">团队:</h3>
+            <a-table :pagination="false" :dataSource="monf.members" :columns="columns" />
+            <h3 class="label">歌曲简介:</h3>
+
+            <p class="intro">{{ monf.intro }}</p>
+            <h3 class="label">歌曲简介:</h3>
+            <BilibiliCe :bvid="monf.bilibiliLink" />
+            <div class="container-footer flex items-center">
+                <div v-if="!appStore.isSelf(monf.createdUserId)" class="control control-like"
+                    :class="{ like: monf.relations?.isLiked }"
+                    @click.stop="monf?.relations?.isLiked ? unLike() : like()">
+                    <i class="bi bi-heart-fill"></i>
+                    {{ monf.likeCount }}
+                </div>
+                <div>
+                    打分
+                </div>
+            </div>
         </div>
+
     </div>
 </template>
 
 <style scoped lang="less">
+.pageHeader {
+    position: sticky;
+    top: 0;
+}
+
 .container {
     padding: 0 14px;
+
+    .title {
+        font-size: 24px;
+        font-weight: bold;
+        margin-bottom: 18px;
+    }
+
+    .intro {
+        line-height: 24px;
+        font-weight: 500;
+        font-size: 16px;
+        margin-top: 12px;
+        max-height: 50vh;
+        margin-bottom: 20px;
+    }
 
     &-body {
         max-width: 100%;
         width: 690px;
         margin: 0 auto;
     }
+
+    &-footer {
+        position: sticky;
+        bottom: 0;
+        height: 50px;
+        padding: 0 20px;
+        column-gap: 14px;
+        background-color: #F9F7F8;
+    }
+}
+
+.control {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50px;
+    width: 70px;
+    height: 30px;
+    background-color: #ddd;
+    font-weight: bold;
+    font-size: 17px;
+    cursor: pointer;
+    color: #5A5A5A;
+
+    i {
+        font-size: 14px;
+        margin-right: 6px;
+    }
+}
+
+.control-like {
+    i {
+        font-size: 13px;
+    }
+}
+
+.like {
+    background-color: #fd4c2c;
+    color: #FFFFFF;
 }
 </style>
